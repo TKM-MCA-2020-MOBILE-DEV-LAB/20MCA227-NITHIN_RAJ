782140a1aece85015be1794693479f0fbd6077ed
Nithin <nithinraj10@hotmail.com>
2022-04-01 12:23:02 +0530

    Auto stash before merge of "master" and "origin/master"

---
+++ CO4/Q3-Themes/app/src/main/java/com/example/themesapp/MainActivity2.java
@@ -0,0 +1,15 @@
+package com.example.themesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setTheme(android.R.style.);
+        setContentView(R.layout.activity_main2);
+    }
+}
