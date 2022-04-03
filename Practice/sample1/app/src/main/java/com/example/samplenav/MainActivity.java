72bdaea7e2249688697e6550e410d2321ee922f9
Nithin <nithinraj10@hotmail.com>
2022-04-03 23:20:34 +0530

    Practice

---
+++ Practice/sample1/app/src/main/java/com/example/samplenav/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.samplenav;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        return super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id==R.id.id1)
+        {
+            Intent i = new Intent(this,MainActivity2.class);
+            startActivity(i);
+        }
+        if(id==R.id.id2)
+        {
+            Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(this,Option2.class);
+            startActivity(i);
+        }
+        if(id==R.id.id3)
+        {
+            Toast.makeText(this, "Option 3 selected", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ Practice/sample2/app/src/main/java/com/example/co1q1/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.co1q1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username,password;
+    Toast toast;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void login(View view)
+    {
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        if(username.getText().toString().equals("Prajitha") && password.getText().toString().equals("5555"))          //Validation
+        {
+            toast = Toast.makeText(getApplicationContext(),"Login Successful",Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        else
+        {
+            Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
